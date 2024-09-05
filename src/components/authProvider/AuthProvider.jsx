f37631b96dfb5ff0c9932a7222b0c0cff06376c3
Niloy11111 <ektiaruddinniloy859@gmail.com>
2024-09-05 14:18:13 +0600

    edited

--- src/components/authProvider/AuthProvider.jsx
+++ src/components/authProvider/AuthProvider.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 
 const AuthContext = createContext(null);
-
+const you = false;
 const AuthProvider = (children) => {
   const { user, setUser } = useState(null);
   return <AuthContext.Provider>{children}</AuthContext.Provider>;
